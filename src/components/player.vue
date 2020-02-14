a6a0f6fd9a416edc8173ddb56335cdb259060591
Atnot Kisel <a6.kiselyov@itransition.com>
2020-02-14 16:41:47 +0300

    base configuration

---
+++ src/components/player.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="player__container">
+        <div class="player__buttons-container">
+            <button>
+                <span class="player__buttons--prev">
+                    prev
+                </span>
+            </button>
+            <button class="player__buttons--play">play</button>
+            <button class="player__buttons--next">next</button>
+        </div>
+        <div class="player__track-description">
+<!--            <img/>-->
+            <div class="player__track-description--title">Title</div>
+            <div class="player__track-description--album">Album</div>
+        </div>
+        <div class="player__progress-track"></div>
+    </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import HelloWorld from './components/HelloWorld';
+import Player from './components/player';
 
-Vue.config.productionTip = false
+Vue.component('world', HelloWorld);
+Vue.component('app-player', Player);
 
 new Vue({
-  render: function (h) { return h(App) },
-}).$mount('#app')
+    render: function (h) {
+        return h(App)
+    },
+}).$mount('#app');
